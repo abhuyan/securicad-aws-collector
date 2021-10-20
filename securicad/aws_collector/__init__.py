bdcbbb60d7a4fe24efe32e52271aa307eafb132a
Max Wällstedt (foreseeti) <48990854+maxwalls@users.noreply.github.com>
2021-10-20 11:59:54 +0200

    Release 1.3.3rc3 (#34)

--- securicad/aws_collector/__init__.py
+++ securicad/aws_collector/__init__.py
@@ -36,7 +36,7 @@ from securicad.aws_collector.exceptions import (
     AwsCredentialsError,
 )
 
-__version__ = "1.3.3rc2"
+__version__ = "1.3.3rc3"
 
 PARSER_VERSION = 8
 PARSER_VERSION_FIELD = "parser_version"
